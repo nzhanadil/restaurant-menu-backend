1617a1a86c93cf3db5174aea51444b125d393dd8
Nuradil Zhanadil uulu <nuradilzhanadiluulu21@gmail.com>
2024-01-10 12:16:41 -0600

    patch

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ server.use(express.json())
 
 server.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
 })
